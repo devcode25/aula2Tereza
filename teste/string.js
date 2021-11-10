250539cdbac0a472939ea0c1d43c266095794caf
devcode25 <alunocode2000@gmail.com>
2021-11-09 21:19:56 -0300

    Criei um novo programinha

---
+++ teste/string.js
@@ -0,0 +1,38 @@
+ 
+
+ //Vamos aprender a concatenar string
+
+ "Hello"
+ 'World'
+
+ //concatenar
+ 
+ /*console.log('Hello' + ' ' + "World")
+
+ console.log('Hello' + "World")
+
+ myName = "Tereza";
+ console.log("Hello" + ' ' + myName)
+
+ myName2 = "2"
+ console.log(`Hello ${myName2}`)*/
+
+//---------------------------
+
+ /*const x = 0
+ y = "morango"
+ console.log(x,"\n",y)*/
+
+// _________________________________
+
+let mes = "dezembro";
+
+if (mes === "fevereiro") {
+    console.log("tem carnaval")
+} else if (mes === "junho") {
+    console.log("tem são joão")
+} else if (mes === "dezembro") {
+    console.log("então é natal")
+} else {
+    console.log(`${mes} não é um mês festivo.`)
+};
